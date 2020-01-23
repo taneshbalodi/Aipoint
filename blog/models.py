bbce438762e0905b21060b39e87af0eea067699b
tanesh balodi <40331022+taneshbalodi@users.noreply.github.com>
2020-01-23 23:01:08 +0700

    some changes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    Title = models.CharField(max_length=260)
+    Publ_date = models.DateTimeField()
+    Body = models.TextField()
+    image = models.ImageField(upload_to="images/")
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import jobs
 # Create your views here.
+def home(request):
+    job = jobs.objects
+    return render(request,'jobs/home.html', {'job': job})
