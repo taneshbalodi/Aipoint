71b436b036dccbbd4883a604253246b236a27a6e
tanesh balodi <40331022+taneshbalodi@users.noreply.github.com>
2020-05-18 08:24:27 +0700

    ss

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from tinymce import HTMLField
+
 from ckeditor.fields import RichTextField
 
 
