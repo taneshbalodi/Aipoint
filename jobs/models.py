5ab8d02859f7b455a859527eced4bdefc9207ca4
tanesh balodi <40331022+taneshbalodi@users.noreply.github.com>
2020-07-08 16:10:37 +0530

    links working

--- jobs/models.py
+++ jobs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from cloudinary.models import CloudinaryField
-
+from django.http import HttpResponse
 from ckeditor.fields import RichTextField
 from django.db.models.signals import pre_save
 from jobs.utils import unique_slug_generator
