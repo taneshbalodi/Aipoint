2a157c07fbdee378441e9f38099ceb89f26332a4
tanesh balodi <40331022+taneshbalodi@users.noreply.github.com>
2020-07-27 09:42:24 +0530

    True

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rclqxj*(z1hyq_og^cg-a!jozo14=n$t0mjm)2aq3x$wp9x-6z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
